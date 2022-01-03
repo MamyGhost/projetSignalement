12540814fa0f79c120333bde2bbdce7b73e78c05
MamyGhost <80816758+MamyGhost@users.noreply.github.com>
2022-01-03 13:56:42 +0300

    update test

---
+++ backoffice/signalement/src/main/java/org/signalement/SignalementApplication.java
@@ -0,0 +1,13 @@
+package org.signalement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalementApplication.class, args);
+	}
+
+}
---
+++ backoffice/signalement/src/test/java/org/signalement/SignalementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.signalement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
