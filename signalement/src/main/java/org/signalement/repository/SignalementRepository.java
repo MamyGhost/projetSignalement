1e7d2d9b26820c6a7d37e93cb622e1ccc5eec8e6
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2022-01-19 19:25:31 +0100

    Update SignalementRepository.java

--- signalement/src/main/java/org/signalement/repository/SignalementRepository.java
+++ signalement/src/main/java/org/signalement/repository/SignalementRepository.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package org.signalement.repository;
+import java.util.Date;
 import java.util.List;
 import org.signalement.entities.Signalement;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,13 @@ import org.springframework.data.repository.query.Param;
  */
 public interface SignalementRepository extends JpaRepository<Signalement, Integer> {
     public List<Signalement>  findByRegionIsNull();
+
+    @Query("select s from Signalement s where s.type.id= :type")
+    public List<Signalement>findSignalementByType(@Param("type") int type);
+    
+    @Query("select s from Signalement s where s.daty= :daty")
+    public List<Signalement>findSignalementByDaty(@Param("daty") String daty);
+    
+    @Query("select s from Signalement s where s.statut.id= :statut")
+    public List<Signalement>findSignalementByStatut(@Param("statut") int statut);
 }
